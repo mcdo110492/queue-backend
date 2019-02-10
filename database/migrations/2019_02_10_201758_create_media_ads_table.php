53f9c2ac594e4f36a555d581ddbead8344480e1b
fuentebellamcdonald@gmail.com <fuentebellamcdonald@gmail.com>
2019-02-10 20:39:28 +0800

    Added Media File Migration

---
+++ database/migrations/2019_02_10_201758_create_media_ads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media_ads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('media_path', 250)->default('default.jpg');
+            $table->integer('weight')->default(1);
+            $table->smallInteger('visibility')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media_ads');
+    }
+}
